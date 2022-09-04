7b899b444c75306c677f4238a0fa4484ff2ce9ab
AN ❦ <108299225+krakxn@users.noreply.github.com>
2022-09-05 00:41:03 +0530

    minor fixes and mock

--- 01-deploy-raffle.js
+++ 01-deploy-raffle.js
@@ -2,7 +2,8 @@ const { getNamedAccounts, deployments, network, run, ethers } = require("hardhat
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify.js")
 
-const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("3")
+// const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("3")
+const VRF_SUB_FUND_AMOUNT = "1000000000000000000000"
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -12,10 +13,10 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     if (developmentChains.includes(network.name)) {
         // create VRFV2 Subscription
-        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
         const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
-        const transactionReceipt = await transactionResponse.wait(1)
+        const transactionReceipt = await transactionResponse.wait()
         subscriptionId = transactionReceipt.events[0].args.subId
         // Fund the subscription
         // Our mock makes it so we don't actually have to worry about sending fund
@@ -30,7 +31,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
     const interval = networkConfig[chainId]["interval"]
 
-    const args = [
+    const arguments = [
         vrfCoordinatorV2Address,
         subscriptionId,
         gasLane,
@@ -41,7 +42,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: args,
+        args: arguments,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
@@ -50,7 +51,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...1")
         //Maybe goerli requires more time to index?  Use:  await new Promise((resolve) => setTimeout(resolve, 60000))
-        await verify(raffle.address, args)
+        await verify(raffle.address, arguments)
     }
 
     log("----------------------------------------------------")
